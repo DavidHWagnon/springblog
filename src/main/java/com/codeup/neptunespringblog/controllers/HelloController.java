196a419a47d9fd4cb54cf2e2131aecd6e86fcb40
David Wagnon <david.wagnon02@gmail.com>
2021-07-21 10:14:42 -0500

    Completed views-lecture

--- src/main/java/com/codeup/neptunespringblog/controllers/HelloController.java
+++ src/main/java/com/codeup/neptunespringblog/controllers/HelloController.java
@@ -1,28 +1,46 @@
 package com.codeup.neptunespringblog.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
-
     @GetMapping("/hello")
     @ResponseBody
-    public String hello(){
+    public String hello() {
         return "<h1>Hello from Spring!</h1>";
     }
 
     @GetMapping("/hello/{name}")
-    @ResponseBody
-    public String sayHello(@PathVariable String name){
-        return "Hello" + name;
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
     }
 
-    @GetMapping("/hello/{num}")
+
+    @GetMapping("/join")
+    public String showJoinForm() {
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
+    }
+
+
+    @GetMapping("/number/{num}")
     @ResponseBody
-    public int displayNumber(@PathVariable int num){
-        return num;
+    public String displayNumber(@PathVariable int num) {
+        // must use the value of method to return the value of the integer as a string object.
+        return String.valueOf(num);
+    }
+
+    @RequestMapping(path = "/hello/in/{color}", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloInColor(@PathVariable String color) {
+        return "<h1 style=\"color: " + color + "\">Hello in " + color + "!</h1>";
     }
 }

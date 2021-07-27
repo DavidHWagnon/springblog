33a8230079104579e841f4ae8ce2126e33e19446
David Wagnon <david.wagnon02@gmail.com>
2021-07-27 14:44:42 -0500

    Completed FBM exercise

--- src/main/java/com/codeup/neptunespringblog/controllers/AdController.java
+++ src/main/java/com/codeup/neptunespringblog/controllers/AdController.java
@@ -1,20 +1,21 @@
 package com.codeup.neptunespringblog.controllers;
-
 import com.codeup.neptunespringblog.models.Ad;
 import com.codeup.neptunespringblog.models.AdRepository;
-import com.codeup.neptunespringblog.models.Post;
+
+import com.codeup.neptunespringblog.models.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 
 @Controller
 public class AdController {
     private final AdRepository adDao;
+    private final UserRepository userDao;
 
-    public AdController(AdRepository adDao) {
+    public AdController(AdRepository adDao, UserRepository userDao) {
         this.adDao = adDao;
+        this.userDao = userDao;
     }
 
     @GetMapping("/ads")
@@ -35,5 +36,34 @@ public class AdController {
         Ad ad = adDao.findByTitle(title);
         model.addAttribute("ad", ad);
         return "ads/show";
+    }
+    // adding a GetMapping to show the form to create a new Ad.
+    @GetMapping("/ads/create")
+    public String createAdForm(Model model) {
+        // by adding our model as an argument, we can add a newly instantiated Ad object to pass to our form.
+        // This will allow the user to directly correlate the fields with our Ad model.
+        model.addAttribute("ad", new Ad());
+        return "ads/create";
+    }
+
+    @PostMapping("/ads/create")
+    public String createAd(@ModelAttribute Ad ad) {
+        // Once the user submits the form to create an Ad, we want to assign a user to the Ad.
+        ad.setUser(userDao.getById(1L));
+        // Since we have our form binding our model data, we can just just save the Ad directly, without having to use RequestParam.
+        adDao.save(ad);
+        return "redirect:/ads";
+    }
+
+    @GetMapping("ads/{id}/edit")
+    public String editAdForm(@PathVariable long id, Model model){
+        Ad adToEdit = adDao.getById(id);
+        model.addAttribute("ad", adToEdit);
+        return "ads/create";
+    }
+
+    @PostMapping("/ads/{id}/edit")
+    public String editAd(@PathVariable long id, @ModelAttribute Ad ad) {
+        return createAd(ad);
     }
 }
